d0938f284ddfa94f8cbd66050c96269fe1c09e64
Theophile <theophile.zamolo@gmail.com>
2022-02-11 16:06:49 +0100

    titre tableau board

--- App/routes.py
+++ App/routes.py
@@ -56,10 +56,10 @@ def board_page():
     Returns:
         [str]: [board page code different if the user is admin or not]
     """
-    admin_candidacy_attributs = ["user_fisrt_name", 'entreprise',
-                                 'contact_full_name', 'contact_email', 'contact_mobilephone', 'date', 'status']
-    usercandidacy_attributs = ['entreprise', 'ville entreprise', 'contact_full_name',
-                               'contact_email', 'contact_mobilephone', 'date', 'status', 'comment']
+    admin_candidacy_attributs = ["Nom", 'entreprise',
+                                 'Nom du contact', 'Email du contact', 'Telephone du contact', 'date', 'statut']
+    usercandidacy_attributs = ['Entreprise', 'Ville entreprise', 'Nom du contact',
+                               'Email du contact', 'Telephone du contact', 'date', 'statut', 'commentaire']
 
     if (current_user.is_admin == True):
         return render_template('board.html', lenght=len(admin_candidacy_attributs), title=admin_candidacy_attributs, user_candidacy=Candidacy.get_all_in_list_with_user_name())
